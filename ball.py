a01e3b76f577f42a648f760905b9144a01f2621a
Olivier JM <manolivier93@gmail.com>
2018-12-31 13:41:26 +0200

    gave the ball all instructions for movement

--- ball.py
+++ ball.py
@@ -17,3 +17,37 @@ class Ball:
         self.circle = self.table.draw_oval(self)
     
     # methods for the ball
+    def start_position(self):
+        self.x_posn = self.x_start
+        self.y_posn = self.y_start
+    
+
+    def start_ball(self, x_speed, y_speed):
+        self.x_speed = -x_speed if random.randint(0, 1) else x_speed
+        self.y_speed = -y_speed if random.randint(0, 1) else y_speed
+        self.start_position()
+    
+    def move_next(self):
+        self.x_posn = self.x_posn + self.x_speed
+        self.y_posn = self.y_posn + self.y_speed
+
+        # check if the ball hits the left side of the wall
+        if (self.x_posn <= 3):
+            self.x_posn = 3
+            self.x_speed = -self.x_speed
+        
+        # check if the ball hits the right side of the wall
+        if(self.x_posn >= (self.table.width - (self.width - 3))):
+            self.x_posn = (self.table.width - (self.width - 3))
+            self.y_speed = -self.y_speed
+        
+        x1 = self.x_posn
+        x2 = self.x_posn + self.width
+        y1 = self.y_posn
+        y2 = self.y_posn + self.height
+        self.table.move_item(self.circle, x1, y1, x2, y2)
+
+    def stop_ball(self):
+        self.x_speed = 0
+        self.y_speed = 0
+
